3d888abb83e6f520ad9716bd2d688adb467212f3
sohil <sohilf88@gmail.com>
2024-03-08 00:37:21 +0530

    selected page pagination added

--- app/user/selected/page.tsx
+++ app/user/selected/page.tsx
@@ -1,16 +1,36 @@
 "use client"
 
+
+import PaginationControls from "@/components/PaginationControls"
 import HlsPlayer from "@/components/hslplayer"
 import { useAppSelector } from "@/reduxtoolkit/store/Hooks"
 
-function page() {
+function page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+    const page = searchParams['page'] ?? '1'
+   const per_page = searchParams['per_page'] ?? '6'
+   const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
+   const end = start + Number(per_page) // 5, 10, 15 ...
+
+  
     const {selectedCamera}=useAppSelector((store)=>store.root.cameras)
+    const entries = selectedCamera.slice(start, end)
+    const hasNext=(end < selectedCamera.length)
+    const hasPrevious=(start>0)
   return (
     <div className="grid grid-cols-3 gap-x-1 gap-y-3 mx-1">
-        <div className='pt-5 pb-5 col-span-full text-center font-semibold text-white text-xl tracking-wider shadow-md px-2'>selected cameras</div>
+        <div className='pt-5 pb-5 col-span-full text-center font-semibold text-white text-xl tracking-wider shadow-md px-2'>
+          <PaginationControls
+           hasNextPage={hasNext}
+           hasPrevPage={hasPrevious}
+          />
+        </div>
       
    {
-    selectedCamera.map((item)=>(
+    entries.map((item)=>(
       <div key={item._id}>
          <HlsPlayer url={item.url}/>
       </div>
